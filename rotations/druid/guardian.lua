4c6b504f1888e7f4a7c65d8ff89f77d26998bd43
ImogenOC <heckoftoday@gmail.com>
2014-01-12 15:47:55 -0500

    Big Guardian Update

--- rotations/druid/guardian.lua
+++ rotations/druid/guardian.lua
@@ -1,4 +1,4 @@
--- SPEC ID 104
+- SPEC ID 104
 ProbablyEngine.rotation.register(104, {
 
   --------------------
@@ -6,44 +6,71 @@ ProbablyEngine.rotation.register(104, {
   --------------------
 
   -- Interrupts
-  { "Skull Bash", "modifier.interrupts" },
-  { "Faerie Fire", "modifier.interrupts" },
-  { "Mighty Bash" , "modifier.interrupts" },
-
-  -- Healing via DoC
-  { "Healing Touch", "player.buff(Dream of Cenarius)", "player" },
-
-  -- Instant Swipe
-  { "Swipe", "modifier.shift" },
+  { "Faerie Fire", "modifier.interrupts", "!modifier.last(Mighty Bash)" },
+  { "Mighty Bash" , "modifier.interrupts", "!modifier.last(Skull Bash)" },
 
   -- Survival
-  { "Savage Defense", { "!player.buff", "player.health > 70" } },
-  { "Barkskin", "player.health < 90" },
-  { "Bone Shield", "player.health < 85" },
-  { "Cenarion Ward" , "player.health < 80", "player" },
-  { "Frenzied Regeneration", { "!player.buff", "player.health < 70" } },
-  { "Might of Ursoc", "player.health < 60" },
-  { "Survival Instincts", "player.health < 50" },
-  { "Renewal", "player.health < 40" },
+  {{
+  { "Renewal", { "player.health <= 30" }},
+  { "Renewal", { "player.health <= 50", "player.buff(Might of Ursoc)" }},
+  { "Survival Instincts", { "player.health <= 40" }},
+  { "Might of Ursoc", { "player.health <= 50" }},
+  { "Barkskin", { "player.health <= 90" }},
+  { "Bone Shield", { "player.buff(Symbiosis)" }},
+  { "Elusive Brew", { "player.health <= 90", "player.buff(Symbiosis)" }},
+  { "#Healthstone", { "player.health <= 50" }},
+  }, { "toggle.Survival" }},
+  { "Frenzied Regeneration", { "player.health <= 80", "!player.buff", "!modifier.last(Frenzied Regeneration)" }},
+  { "Savage Defense", { "player.health <= 95", "!player.buff", "!modifier.last(Savage Defense)" }},
+  { "Healing Touch", { "player.health <= 95", "player.buff(Dream of Cenarius)" }},
+  
+  --AOE
+  { "Swipe", { "modifier.multitarget", "target.range <= 5" }}, 
 
   -- Cooldowns
-  { "Incarnation: Son of Ursoc", { "!player.buff(Incarnation: Son of Ursoc)", "modifier.cooldowns" }},
-  { "Natures Vigil" },
+  {{
+  { "Nature's Vigil", { "player.spell(Berserk).cooldown = 0" }},
+  { "Nature's Vigil", { "player.spell(Incarnation: Son of Ursoc).cooldown = 0" }},
+  { "Incarnation: Son of Ursoc", { "player.buff(Nature's Vigil)" }},
+  { "Berserk", { "player.time > 5", "!player.buff(Incarnation: Son of Ursoc)", "player.spell(Nature's Vigil).duration > 10" }},
+  },  { "target.range <= 5", "player.spell(Nature's Vigil).exists", "modifier.cooldowns" }},
+  {{
+  { "Incarnation: Son of Ursoc" },
+  { "Berserk", { "player.time >= 90", "!player.buff(Incarnation: Son of Ursoc)" }},
+  }, { "target.range <= 5", "!player.spell(Nature's Vigil).exists", "modifier.cooldowns" }},
 
   -- Mob Control
-  { "Growl", "modifier.taunt" },
-
-  { "Swipe", "modifier.multitarget" },
-  { "Enrage", "player.rage < 20" },
+  { "Enrage", { "player.rage < 70" }},
+  { "Faerie Fire", { "!target.debuff(Weakened Armor)" }},
+  { "Thrash", { "target.range <= 5", "target.debuff.duration < 3" }},
+  { "Maul", { "player.buff(Tooth and Claw)", "!modifier.last(Maul)" }},
+  { "Maul", {"player.rage >= 70", "!modifier.last(Maul)"  }},
   { "Mangle" },
-  { "Thrash", "target.debuff.duration < 4" },
-  { "Faerie Fire", "!target.debuff(Weakened Armor)" },
-  { "Lacerate" },
-  { "Maul", "player.buff(Tooth and Claw)" },
-  { "Maul", "player.rage >= 85" },
+  { "Lacerate", { "target.debuff.duration < 3", "!modifier.multitarget" }},
+  { "Lacerate", { "!modifier.last(Lacerate)" }},
+  { "Swipe", { "target.range <= 5", "modifier.enemies > 3" }},
+  { "Faerie Fire" },
+  { "Thrash", { "target.range <= 5" }},
+  { "Swipe", { "target.range <= 5" }},
 
   ------------------
   -- End Rotation --
   ------------------
 
-})
+},
+{
+
+  ---------------
+  -- OOC Begin --
+  ---------------
+
+{ "Mark of the Wild", { "!lowest.buff(Mark of the Wild).any", "!lowest.buff(Blessing of Kings).any", "!lowest.buff(Legacy of the Emperor).any", "lowest.range <= 30", "player.form = 0" }, "lowest" }
+
+  -------------
+  -- OOC End --
+  -------------
+
+},
+function()
+ProbablyEngine.toggle.create('Survival', 'Interface\\Icons\\Ability_druid_tigersroar','Survivability','Enable or Disable the usage of Survivability Cooldowns')
+end)
