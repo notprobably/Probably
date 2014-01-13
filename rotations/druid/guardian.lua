53e6aa803681f8ed41eb166d6231aac1342e0f5b
ImogenOC <heckoftoday@gmail.com>
2014-01-13 15:56:45 -0500

    Thing?

--- rotations/druid/guardian.lua
+++ rotations/druid/guardian.lua
@@ -5,10 +5,17 @@ ProbablyEngine.rotation.register(104, {
   -- Start Rotation --
   --------------------
 
+  --Feck you need bear form
+  { "Bear Form", { "!player.buff(Bear Form)" }},
+
   -- Interrupts
   { "Faerie Fire", "modifier.interrupts", "!modifier.last(Mighty Bash)" },
   { "Mighty Bash" , "modifier.interrupts", "!modifier.last(Skull Bash)" },
 
+  -- Ress target-Instantly
+  { "Rebirth", { "target.dead", "player.buff(Dream of Cenarius)" }, "target" },
+  { "Rebirth", { "mouseover.dead", "player.buff(Dream of Cenarius)" }, "mouseover" },
+  
   -- Survival
   {{
   { "Renewal", { "player.health <= 30" }},
@@ -22,7 +29,9 @@ ProbablyEngine.rotation.register(104, {
   }, { "toggle.Survival" }},
   { "Frenzied Regeneration", { "player.health <= 80", "!player.buff", "!modifier.last(Frenzied Regeneration)" }},
   { "Savage Defense", { "player.health <= 95", "!player.buff", "!modifier.last(Savage Defense)" }},
-  { "Healing Touch", { "player.health <= 95", "player.buff(Dream of Cenarius)" }},
+  { "Healing Touch", { "player.health <= 90", "player.buff(Dream of Cenarius)" }}, --self
+  { "Healing Touch", { "targettarget.health <= 90", "player.buff(Dream of Cenarius)" }, "targettarget" }, --self or other tank
+  { "Healing Touch", { "lowest.health <= 65", "player.buff(Dream of Cenarius)" }, "lowest" }, --anyone in raid
   
   --AOE
   { "Swipe", { "modifier.multitarget", "target.range <= 5" }}, 
@@ -33,18 +42,19 @@ ProbablyEngine.rotation.register(104, {
   { "Nature's Vigil", { "player.spell(Incarnation: Son of Ursoc).cooldown = 0" }},
   { "Incarnation: Son of Ursoc", { "player.buff(Nature's Vigil)" }},
   { "Berserk", { "player.time > 5", "!player.buff(Incarnation: Son of Ursoc)", "player.buff(Nature's Vigil).duration > 10" }},
-  },  { "target.range <= 5", "player.spell(Nature's Vigil).exists", "modifier.cooldowns" }},
+  }, { "!target.dead", "target.range <= 5", "player.spell(Nature's Vigil).exists", "modifier.cooldowns" }},
   {{
   { "Incarnation: Son of Ursoc" },
   { "Berserk", { "player.time >= 90", "!player.buff(Incarnation: Son of Ursoc)" }},
-  }, { "target.range <= 5", "!player.spell(Nature's Vigil).exists", "modifier.cooldowns" }},
+  }, { "!target.dead", "target.range <= 5", "!player.spell(Nature's Vigil).exists", "modifier.cooldowns" }},
 
   -- Mob Control
   { "Enrage", { "player.rage < 70" }},
-  { "Faerie Fire", { "!target.debuff(Weakened Armor)" }},
+  { "Faerie Fire", { "target.debuff(Weakened Armor).duration < 3" }},
   { "Thrash", { "target.range <= 5", "target.debuff.duration < 3" }},
   { "Maul", { "player.buff(Tooth and Claw)", "!modifier.last(Maul)" }},
-  { "Maul", {"player.rage >= 70", "!modifier.last(Maul)"  }},
+  { "Maul", { "player.rage >= 70", "!modifier.last(Maul)"  }},
+  { "Maul", { "player.treat < 100", "!modifier.last(Maul)"  }},
   { "Mangle" },
   { "Lacerate", { "target.debuff.duration < 3", "!modifier.multitarget" }},
   { "Lacerate", { "!modifier.last(Lacerate)" }},
