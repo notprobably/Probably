6b7d97507bb2c3b2c8e7057f14cc797c80b59479
Ben Phelps <bphelpsen@gmail.com>
2014-01-20 12:22:32 -0600

    Fix rotation errors

--- rotations/rogue/NewbRogue.lua
+++ rotations/rogue/NewbRogue.lua
@@ -4,35 +4,35 @@ ProbablyEngine.rotation.register(4, {
   --------------------
   -- Start Rotation --
   --------------------
-  
+
   -- Keybinds
   { "Throw", "modifier.shift" },
-  
+
   -- Survival
   { "Evasion", "player.health <= 75" },
-  
+
   -- Opener
-  { "Stealth", { 
-    "target.enemy"
+  { "Stealth", {
+    "target.enemy",
     "!player.buff(Stealth)",
   }},
-  
+
   { "Ambush", "player.buff(Stealth)" },
-  
+
   -- Rotation
   { "Eviscerate", "player.combopoints >= 4" },
   { "Sinister Strike" },
-  
+
   ------------------
   -- End Rotation --
   ------------------
-  
+
   },{
   -- Opener
-  { "Stealth", { 
-    "target.enemy"
+  { "Stealth", {
+    "target.enemy",
     "!player.buff(Stealth)",
   }},
-  
+
   { "Ambush", "player.buff(Stealth)" },
 })
